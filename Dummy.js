54440052330442ba9826e419f1484b5216407229
aasuquo_samuel <samuelasuquo80@gmail.com>
2023-06-13 12:51:24 +0100

    first commit

--- Dummy.js
+++ Dummy.js
@@ -1,31 +1,62 @@
 import React from "react";
 import { StyleSheet, Image, Dimensions, Text, View } from "react-native";
-import AntDesign from "@expo/vector-icons/AntDesign"
+import AntDesign from "@expo/vector-icons/AntDesign";
 import Swiper from "react-native-swiper";
 // import AppLoading from "expo-app-loading";
 import {
   useFonts,
   Montserrat_400Regular,
   Montserrat_500Medium,
-  Montserrat_600SemiBold,
+  montserrat_600SemiBold,
   Montserrat_700Bold,
-} from "@expo-google-fonts/montserrat"
+} from "@expo-google-fonts/montserrat";
 
+const w = Dimensions.get("window").width;
+const h = Dimensions.get("window").height;
 
-const w = Dimensions.get("window").width
-const h = Dimensions.get("window").height
-
+const styles = StyleSheet.create({
+  slide: {
+    flex: 1,
+    paddingTop: 80,
+    marginHorizontal: 30,
+  },
+  img: {
+    alignSelf: "center",
+    borderTopRightRadius: 80,
+    borderBottomLeftRadius: 80,
+    height: h * 0.5,
+    width: w * 0.9,
+  },
+  title: {
+    fontFamily: "Montserrat_700Bold",
+    marginTop: 60,
+    marginHorizontal: 10,
+    fontSize: 32,
+  },
+  text: {
+    color: "#767676",
+    fontFamily: "Montserrat_400Regular",
+    marginTop: 20,
+    fontSize: 16,
+    lineHeight: 25,
+    marginLeft: 10,
+  },
+});
 
 const App = () => {
-  let [fontLoaded] = useFonts({
+  let [fontsLoaded] = useFonts({
     Montserrat_400Regular,
     Montserrat_500Medium,
-    Montserrat_600SemiBold,
+    montserrat_600SemiBold,
     Montserrat_700Bold,
-  })
-    return (
+  });
+
+  // if (!fontsLoaded) {
+  //   return <AppLoading />;
+  // } else {
+  return (
       <Swiper
-       buttonWrapperStyle={{
+        buttonWrapperStyle={{
           backgroundColor: "transparent",
           flexDirection: "row",
           position: "absolute",
@@ -41,19 +72,21 @@ const App = () => {
         showsButtons={true}
         paginationStyle={{
           marginRight: w * 0.7,
-          marginBottom: w * 0.02,
+          marginBottom: h * 0.02,
         }}
-        activeDotColors="#8A56AC"
+        activeDotColor="#8A56AC"
         dotColor="#998FA2"
         nextButton={
-          <View style={{
-            height: 60,
-            borderRadius: 30,
-            alignItems: "center",
-            justifyContent: "center",
-            width: 60,
-            backgroundColor: "#8A56AC"
-          }}>
+          <View
+            style={{
+              height: 60,
+              borderRadius: 30,
+              alignItems: "center",
+              justifyContent: "center",
+              width: 60,
+              backgroundColor: "#8A56AC",
+            }}
+          >
             <AntDesign name="arrowright" size={22} color="#FFF" />
           </View>
         }
@@ -68,24 +101,22 @@ const App = () => {
               backgroundColor: "#8A56AC",
               marginHorizontal: 20,
             }}
-        >
-
+          >
             <AntDesign name="arrowleft" size={22} color="#FFF" />
           </View>
         }
       >
         <View style={styles.slide}>
-          <Image source={require('./images/img.png')} style={styles.img} />
-          <Text style={styles.title}>Meet up Ui Toolkit</Text>
+          <Image source={require("./images/img.png")} style={styles.img} />
+          <Text style={styles.title}>Meet Up UI-Kit</Text>
           <Text style={styles.text}>
             When I was 5 years old, my mother always told me that happiness was
             the key to life. When I went to school, they asked me what I wanted
             to be when I grew up.
           </Text>
         </View>
-
         <View style={styles.slide}>
-          <Image source={require('./images/img2.png')} style={styles.img} />
+          <Image source={require("./images/img2.png")} style={styles.img} />
           <Text style={styles.title}>Discover</Text>
           <Text style={styles.text}>
             When I was 5 years old, my mother always told me that happiness was
@@ -95,7 +126,7 @@ const App = () => {
         </View>
 
         <View style={styles.slide}>
-          <Image source={require('./images/img3.png')} style={styles.img} />
+          <Image source={require("./images/img3.png")} style={styles.img} />
           <Text style={styles.title}>Get Started</Text>
           <Text style={styles.text}>
             When I was 5 years old, my mother always told me that happiness was
@@ -103,9 +134,8 @@ const App = () => {
             to be when I grew up.
           </Text>
         </View>
-
         <View style={styles.slide}>
-          <Image source={require('./images/img4.png')} style={styles.img} />
+          <Image source={require("./images/img4.png")} style={styles.img} />
           <Text style={styles.title}>Welcome</Text>
           <Text style={styles.text}>
             When I was 5 years old, my mother always told me that happiness was
@@ -114,35 +144,8 @@ const App = () => {
           </Text>
         </View>
       </Swiper>
-    )
-}
+  );
+  // }
+};
 
-const styles = StyleSheet.create({
-  slide: {
-    paddingTop: 60,
-    marginHorizontal: 30,
-  },
-  img: {
-    alignSelf: 'center',
-    borderTopRightRadius: 80,
-    borderBottomLeftRadius: 80,
-    height: h * 0.5,
-    width: w * 0.9,
-  },
-  title: {
-    fontFamily: "Montserrat_700Bold",
-    marginTop: 40,
-    marginHorizontal: 10,
-    fontSize: 32,
-  },
-  text: {
-    color: "#767676",
-    fontFamily: "Montserrat_400Regular",
-    marginTop: 20,
-    fontSize: 16,
-    lineHeight: 25,
-    marginLeft: 10,
-  }
-})
-
-export default App
+export default App;
